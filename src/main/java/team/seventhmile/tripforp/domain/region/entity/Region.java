072f16b8a66297bf3430d8a55b906879876fc683
junggyun <onlyplsson@gmail.com>
2024-12-31 02:52:04 +0900

    feat: Region 상위지역, 하위지여 유니크 제약 추가

--- src/main/java/team/seventhmile/tripforp/domain/region/entity/Region.java
+++ src/main/java/team/seventhmile/tripforp/domain/region/entity/Region.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,7 +17,8 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "regions")
+@Table(name = "regions",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"province", "city"}))
 public class Region {
 
     @Id
