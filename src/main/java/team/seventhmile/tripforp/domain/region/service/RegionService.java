5ccf5b9289932bdc6cb9e15f88eeceb51d40ed99
junggyun <onlyplsson@gmail.com>
2025-01-03 04:54:26 +0900

    feat: 하위지역 조회 캐시 적용

--- src/main/java/team/seventhmile/tripforp/domain/region/service/RegionService.java
+++ src/main/java/team/seventhmile/tripforp/domain/region/service/RegionService.java
@@ -2,7 +2,14 @@ package team.seventhmile.tripforp.domain.region.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.seventhmile.tripforp.domain.region.dto.CreateRegionRequest;
@@ -14,11 +21,13 @@ import team.seventhmile.tripforp.global.exception.ResourceNotFoundException;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class RegionService {
 
     private final RegionRepository regionRepository;
 
     @Transactional
+    @CacheEvict(value = "regions", key = "#request.province")
     public Long createRegion(CreateRegionRequest request) {
         Region region = Region.builder()
             .province(Province.findByName(request.getProvince()))
@@ -30,11 +39,15 @@ public class RegionService {
     }
 
     @Transactional
-    public void deleteRegion(Long id) {
+    @CacheEvict(value = "regions", key = "#result.province.name")
+    public Region deleteRegion(Long id) {
         Region region = regionRepository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException(Region.class, id));
+
         regionRepository.delete(region);
+        return region;
     }
+
 
     public List<String> getProvinceList() {
         List<String> result = new ArrayList<>();
@@ -44,6 +57,7 @@ public class RegionService {
         return result;
     }
 
+    @Cacheable(value = "regions", key = "#province")
     public List<String> getCityListByProvince(String province) {
         List<Region> regions = regionRepository.findByProvince(Province.findByName(province));
         List<String> result = new ArrayList<>();
