a41266ffbe5553be3126a141d39a777b4ce9c120
junggyun <onlyplsson@gmail.com>
2025-01-04 06:20:06 +0900

    feat: 구글 텍스트 검색 api 지역 정보 추가

--- src/main/java/team/seventhmile/tripforp/external/google/dto/SearchPlacesApiRequest.java
+++ src/main/java/team/seventhmile/tripforp/external/google/dto/SearchPlacesApiRequest.java
@@ -1,5 +1,6 @@
 package team.seventhmile.tripforp.external.google.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,5 +14,36 @@ public class SearchPlacesApiRequest {
     private final String regionCode = "kr";
     private int pageSize;
     private String pageToken;
+    private LocationBias locationBias = new LocationBias(
+        new Circle(
+            new Center(36.6357, 127.4912),
+            50000.0
+        )
+    );
 
+}
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class LocationBias {
+    private Circle circle;
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class Circle {
+    private Center center;
+    private double radius;
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class Center {
+    private double latitude;
+    private double longitude;
 }
